93f77a169dd71197de028060a026471dc8f9c741
graeme <graeme@lolux.net>
2017-04-02 19:30:59 +1200

    Update vem_server.py
    add msazure support

--- vem_server.py
+++ vem_server.py
@@ -31,14 +31,15 @@ import math
 import sys
 import time
 import json
+import subprocess
 
+# osc
 from pythonosc import dispatcher
 from pythonosc import osc_server
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
 # internal api
-import base64
 import dynamic
 import analysis_2
 import os
@@ -46,16 +47,22 @@ import tempfile
 
 # voiceit
 from voiceit import *
+# msazure
+from msazure import *
 
 # server and client
 global vox_server
 global vox_udp
 global count
-global email 
+global userid 
 global password
 global voice
+global azure
 
 
+# utility messages ############################################################
+
+# stop server
 def exit_handler(unused_addr, args, localmsg):
     msg = osc_message_builder.OscMessageBuilder(address = '/vox/server')
     msg.add_arg("vox> server...exit")
@@ -64,25 +71,33 @@ def exit_handler(unused_addr, args, localmsg):
     vox_server.shutdown()
 
 
+# helo return version 
 def helo_handler(unused_addr, args, localmsg):
     msg = osc_message_builder.OscMessageBuilder(address = '/vox/server')
     msg.add_arg("vox> server..ok")
     msg = msg.build()
     vox_udp.send(msg)
 
+# test osc comms is working 
+def test_handler(unused_addr, args, filename):
+    msg = osc_message_builder.OscMessageBuilder(address = '/vox/server')
+    msg.add_arg("vox> server...ack")
+    msg = msg.build()
+    vox_udp.send(msg)
 
-def voiceit_user_handler(unused_addr, args, user):
-	# TODO: set username adn password here
-	if user == 0:
-		email = "example@email.com"
-		password = "example-password"
-	print(email)
+
+# select userid for any API
+def user_handler(unused_addr, args, user):
+	# set username
+	global userid
+	userid = user
+	print(userid)
 
 
+# voice it api ###################################################################
 def voiceit_auth_handler(unused_addr, args, filename):
-	# voiceit authentication attempt voiceit api	
-	print("authenticating")
-	vox_result = json.loads(voice.authentication(email, password, filename))
+	print("authenticating " + userid)
+	vox_result = json.loads(voice.authentication(userid, password, filename))
 	vox_code = vox_result['ResponseCode']
 	fileinfo = os.path.split(filename)
 	vox_msg = "".join(["vit> ", fileinfo[1], " ", vox_result['Result']])
@@ -94,9 +109,8 @@ def voiceit_auth_handler(unused_addr, args, filename):
 
 	
 def voiceit_enrol_handler(unused_addr, args, filename):
-	# voiceit enrollment attempt voiceit api
-	print("enrolling")	
-	vox_result = json.loads(voice.createEnrollment(email, password, filename))
+	print("enrolling " + userid)	
+	vox_result = json.loads(voice.createEnrollment(userid, password, filename))
 	vox_code = vox_result['ResponseCode']
 	fileinfo = os.path.split(filename)
 	vox_msg = "".join(["vit> ", fileinfo[1], " ", vox_result['Result']])
@@ -107,11 +121,36 @@ def voiceit_enrol_handler(unused_addr, args, filename):
 	vox_udp.send(msg)
 
 
-def internal_handler(unused_addr, args, filename):
-	# hackbright internal API 
-	# TODO: define wav file for comparison
-	template = "example.wav"
+# ms azure api ######################################################################
+def msazure_auth_handler(unused_addr, args, filename): 
+	print("authenticating " + userid)
+	vox_result = json.loads(azure.authentication(userid, password, filename))
+	fileinfo = os.path.split(filename)
+	# TODO: check for HTTP 200 or 500 with error msg
+	vox_msg = "".join(["msa> ", fileinfo[1], " ", vox_result['result'], " ", vox_result['confidence']])
 	
+	msg = osc_message_builder.OscMessageBuilder(address = '/vox/server')
+	msg.add_arg(vox_msg)
+	msg = msg.build()
+	vox_udp.send(msg)
+
+	
+def msazure_enrol_handler(unused_addr, args, filename):
+	print("enrolling " + userid)
+	vox_result = json.loads(azure.createEnrollment(userid, password, filename))
+	fileinfo = os.path.split(filename)
+	# TODO: check for HTTP 200 or 500 with error msg
+	vox_msg = "".join(["msa> ", fileinfo[1], " ", vox_result['enrollmentStatus']])
+	
+	msg = osc_message_builder.OscMessageBuilder(address = '/vox/server')
+	msg.add_arg(vox_msg)
+	msg = msg.build()
+	vox_udp.send(msg)
+
+# internal api #######################################################################
+def internal_auth_handler(unused_addr, args, filename):
+	# pyvox internal API comparison
+	template = "/home/graeme/Research/voxexmachina/sc/never.wav"
 	seqx = analysis_2.master(template)
 	seqy = analysis_2.master(filename)
 	cost = dynamic.dynamicTimeWarp(seqx, seqy)
@@ -127,15 +166,14 @@ def internal_handler(unused_addr, args, filename):
 	msg = msg.build()
 	vox_udp.send(msg)
 
+
+def internal_enrol_handler(unused_addr, args, filename):
+	#enrol internal api
+	print("enrolling " + userid)
+	# TODO select file and overwrite user template file for comparison
 	
-def test_handler(unused_addr, args, filename):
-    # test OSC comms works
-    msg = osc_message_builder.OscMessageBuilder(address = '/vox/server')
-    msg.add_arg("vox> server...ack")
-    msg = msg.build()
-    vox_udp.send(msg)
-	
-	
+
+# main loop #############################################################################
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--ip", default="127.0.0.1", help="The IP to listen on")
@@ -144,23 +182,40 @@ if __name__ == "__main__":
 
 	# initialise VoiceIt API 
 	voice = VoiceIt()
-	# TODO set default username and password
-	password = ""
-	email = "example@email.com"
-	
+	#initialise the MSAzure API 
+	azure = MSAzure()
+
+	# user
+	userid = "*"
+	# TODO: hardcoded in voiceit / msazure doesn't need it...?
+	password=""
+
 	# client listening on local ip only
 	vox_udp = udp_client.SimpleUDPClient('127.0.0.1', 57120)
+	# client listening on network ip
+	#vox_udp = udp_client.SimpleUDPClient('10.20.15.117', 57120)
 	
-	# osc addresses that we isten to
 	dispatcher = dispatcher.Dispatcher()
 	dispatcher.map("/vox/helo", helo_handler, "helo")
 	dispatcher.map("/vox/exit", exit_handler, "exit")
 	dispatcher.map("/vox/test", test_handler, "test")
-	dispatcher.map("/vox/api/internal", internal_handler, "internal")
-	dispatcher.map("/vox/api/voiceit/auth", voiceit_auth_handler, "voiceit_auth")
-	dispatcher.map("/vox/api/voiceit/user", voiceit_user_handler, "voiceit_user")
-	dispatcher.map("/vox/api/voiceit/enrol", voiceit_enrol_handler, "voiceit_enrol")
-	
+
+	# user_id selected for api
+	dispatcher.map("/vox/api/user", user_handler, "api_user")
+	# internal API
+	dispatcher.map("/vox/api/auth/internal", internal_auth_handler, "internal_auth")
+	dispatcher.map("/vox/api/enrol/internal", internal_enrol_handler, "internal_enrol")
+	# voiceit API
+	dispatcher.map("/vox/api/auth/voiceit", voiceit_auth_handler, "voiceit_auth")
+	dispatcher.map("/vox/api/enrol/voiceit", voiceit_enrol_handler, "voiceit_enrol")
+	# ms azure API
+	dispatcher.map("/vox/api/auth/msazure", msazure_auth_handler, "msazure_auth")
+	dispatcher.map("/vox/api/enrol/msazure", msazure_enrol_handler, "msazure_enrol")
+
+	# make the server
 	vox_server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
 	vox_server.serve_forever()
+	
+
+
 	
